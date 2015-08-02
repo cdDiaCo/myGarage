1243d6a8f0ca819e988e5a824c403f7b3c34dc58
Bogdan Cornianu <bogdan.cornianu@gmail.com>
2015-08-02 15:38:05 +0300

    Add django rest framework.

--- myGarage/urls.py
+++ myGarage/urls.py
@@ -1,34 +1,18 @@
-from myGarageClient.views import user_logout, home_page, addNewCar, garageView, carCleanings, carRefuellings, carServices, \
-    carRevisions, carTaxes, ajaxView, carInsurances
+# from myGarageClient.views import user_logout, home_page, addNewCar, garageView, carCleanings, carRefuellings, carServices, \
+#     carRevisions, carTaxes, ajaxView, carInsurances
+# from myGarageClient.views import user_logout, home_page, garageView
 from django.contrib import admin
-from django.conf.urls import patterns, url, include
-from myGarageClient import views
+from django.conf.urls import patterns, url
 from django.conf.urls import include
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myGarage.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^api/v1/', include('myGarageApi.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^register/$', register),
-    #url(r'^login/$', user_login),
-    url(r'^logout/$', user_logout),
-    #url(r'^home/$', home_page),
-    url(r'^new_car/$', addNewCar),
-    #url(r'^my_cars/$', myCars),
-    #url(r'^my_cars/\d+/$', myCars),
-    url(r'^cleanings/$', carCleanings),
-    url(r'^refuellings/$', carRefuellings),
-    url(r'^service/$', carServices),
-    url(r'^revisions/$', carRevisions),
-    url(r'^taxes/$', carTaxes),
-    url(r'^insurances', carInsurances),
-    url(r'^ajaxView', ajaxView),
-    url(r'^garage/$', garageView),
-    url(r'^garage/(\d{1})/$', garageView),    
-    url(r'^$', home_page),
+    # url(r'^logout/$', user_logout),
+    # # url(r'^garage/$', garageView),
+    # # url(r'^garage/(\d{1})/$', garageView),
+    # url(r'^$', home_page),
 )
 
 
--- myGarageApi/views.py
+++ myGarageApi/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from .models import Car, Refuelling, Cleaning, Service, Revision, Tax, Insurance, Tyre
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import CarSerializer, UserSerializer, RefuellingSerializer, CleaningSerializer, ServiceSerializer, \
+    RevisionSerializer, TaxSerializer, InsuranceSerializer, TyreSerializer
+from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(username=user)
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Car.objects.filter(user__username=user)
+
+
+class RefuellingViewSet(viewsets.ModelViewSet):
+    queryset = Refuelling.objects.all()
+    serializer_class = RefuellingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Refuelling.objects.filter(car__user__username=user)
+
+
+class CleaningViewSet(viewsets.ModelViewSet):
+    queryset = Cleaning.objects.all()
+    serializer_class = CleaningSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Cleaning.objects.filter(car__user__username=user)
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Service.objects.filter(car__user__username=user)
+
+
+class RevisionViewSet(viewsets.ModelViewSet):
+    queryset = Revision.objects.all()
+    serializer_class = RevisionSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Revision.objects.filter(car__user__username=user)
+
+
+class TaxViewSet(viewsets.ModelViewSet):
+    queryset = Tax.objects.all()
+    serializer_class = TaxSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tax.objects.filter(car__user__username=user)
+
+
+class InsuranceViewSet(viewsets.ModelViewSet):
+    queryset = Insurance.objects.all()
+    serializer_class = InsuranceSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Insurance.objects.filter(car__user__username=user)
+
+
+class TyreViewSet(viewsets.ModelViewSet):
+    queryset = Tyre.objects.all()
+    serializer_class = TyreSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Tyre.objects.filter(car__user__username=user)
