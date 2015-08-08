040d31697b8cb2369f4a733b260ba950d1386f53
Bogdan Cornianu <bogdan.cornianu@gmail.com>
2015-08-08 17:55:33 +0300

    Split settings file into base, local and production.

---
+++ myGarage/settings/base.py
@@ -0,0 +1,86 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+import json
+from django.core.exceptions import ImproperlyConfigured
+
+# JSON-based secrets module
+with open("secrets.json") as f:
+    secrets_file = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets_file):
+    """Get the secret variable or return explicit exception."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {0} environment variable".format(setting)
+    raise ImproperlyConfigured(error_msg)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'myGarageApi/templates').replace('\\','/'),
+    os.path.join(os.path.dirname(__file__), 'myGarageClient/templates').replace('\\','/'),
+)
+
+ALLOWED_HOSTS = []
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'myGarageClient',
+    'myGarageApi',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'myGarage.urls'
+
+WSGI_APPLICATION = 'myGarage.wsgi.application'
+
+#AUTH_PROFILE_MODULE = 'myGarage.UserProfile'
+
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10,
+    'PAGINATE_BY_PARAM': 'page_size',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Bucharest'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/myGarageClient/static/'
