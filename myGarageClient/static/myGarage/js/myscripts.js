0e18c0ac894a516b4f47df5463e65c24ab92c2a6
claudia <claudia_cocioaba@yahoo.ro>
2015-08-09 19:53:26 +0300

    rewrite table functions

--- myGarageClient/static/myGarage/js/myscripts.js
+++ myGarageClient/static/myGarage/js/myscripts.js
@@ -590,13 +590,13 @@ function saveRowFunction(e) {
     }
 }
 
-
+var activeBtnState = {};
 
 function setButtonActive(elem) {
     deactivateButtons();
     $(elem).addClass('active');
     removeTable();
-    generateTable();
+    prepareTable();
 }
 
 function deactivateButtons() {
@@ -630,11 +630,11 @@ function getUserCars() {
             }
 
             $('#refuellingsButton').addClass('active');
-            generateTable();
+            prepareTable();
         });
 }
 
-function generateTable() {
+function prepareTable() {
     // check what section button has the active class
     //generate table for that section
     var sectionBtn = $('.sectionsButton.active');
@@ -642,11 +642,10 @@ function generateTable() {
     var index = sectionID.indexOf("Button");
     var sectionName = sectionID.substring(0, index);
     sectionName = sectionName.toLowerCase();
-    console.log(sectionName);
-    getTableRecords(sectionName);
+    getTableColumns(sectionName);
+
 }
 
-var tableStateObj = {};
 
 function getTableRecords(sectionName) {
      $.ajax({type:"GET", url: "/api/v1/"+sectionName+"/"})
@@ -654,76 +653,96 @@ function getTableRecords(sectionName) {
             console.log('bad credentials.');
         })
         .done(function(resp){
-            if(!resp.count){
-                 tableStateObj.results = [];
-                 var message = "there are no "+ sectionName+ " records yet";
-                 $('#garageContent').append(message);
-            }
-            else {
-                $('#tableRecords thead').append($('<tr>'));
-                tableStateObj.tableHeadCellsAdded = false;
-                tableStateObj.results = resp.results;
-                tableStateObj.sectionName = sectionName;
-                tableStateObj.addNewRow = addNewRow;
-                tableStateObj.addNewRow();
-            }
-        });
+            console.log('in get table records');
+            activeBtnState.sectionName = sectionName;
+            activeBtnState.numOfRecords = resp.count;
+            activeBtnState.results = resp.results;
+            addTableBody();
+     });
 }
 
+function getTableColumns(sectionName) {
+    var sectionSingular;
+    if(sectionName === "taxes") {
+        sectionSingular = sectionName.substring(0, sectionName.length-2);
+    }
+    else {
+        sectionSingular = sectionName.substring(0, sectionName.length-1);
+    }
+    $.ajax({type:"GET", url: "/api/v1/columns/"+sectionSingular+"/"})
+        .fail(function(resp){
+            console.log('bad credentials.');
+        })
+        .done(function(resp){
+            console.log('in get table columns');
+            activeBtnState.numOfColumns = resp.length-2;
+            activeBtnState.columns = resp;
+            addTableHead();
+            getTableRecords(sectionName);
+    });
+}
 
-function addNewRow() {
-     var self = this;
-     var carHasRecords = false;
+function addTableHead() {
+    $('#tableRecords thead').append($('<tr>'));
+    var tableHead = $('#tableRecords thead').find('tr');
+    for(column in activeBtnState.columns) {
+        var columnName = getTableHeadName(activeBtnState.columns[column]);
+        if(columnName === "Id" || columnName === "Car") {
+            continue;
+        }
+        tableHead.append($('<th>').text(columnName));
+    }
+}
 
-     $.each(self.results, function(index, elem) {
-            var tableContainerWidth = $('#garageContent').width();
-            var tableHead = $('#tableRecords thead').find("tr");
-            $('#tableRecords tbody').append($('<tr>'));
-            var newRow = $('#tableRecords tbody').find('tr').last();
-            var numOfColumns = 0;
+function addTableBody() {
+    if(activeBtnState.numOfRecords > 0){
+         var carHasRecords = false;
+         $.each(activeBtnState.results, function(index, elem) {
+             var tableContainerWidth = $('#garageContent').width();
+             $('#tableRecords tbody').append($('<tr>'));
+             var newRow = $('#tableRecords tbody').find('tr').last();
+             for (var key in elem) {
+                 if(key==="pk") {continue;}
+                 if(key==="car") {
+                      if(!isSelectedCar(elem[key])) {
+                          if(!carHasRecords && index === (activeBtnState.results.length-1)) {
+                              var message = 'You have no '+activeBtnState.sectionName+' added for this car!';
+                              setNoRecordsBody(newRow, message);
+                          }
+                          return;
+                      }
+                      carHasRecords = true;
+                      continue;
+                 }
+                 $(newRow).append($('<td>')
+                          .append($('<input>')
+                               .prop('type', 'text')
+                               .attr('name', key)
+                               .val(elem[key])
+                          ));
+             }
+             var tableCell = $('td');
+             var leftPadding = tableCell.css('padding-left');
+             var rightPadding = tableCell.css('padding-right');
+             var widthPadding = parseInt(leftPadding.charAt(0)) + parseInt(rightPadding.charAt(0));
+             var cellWidth = tableContainerWidth/activeBtnState.numOfColumns-(widthPadding+1);
+             $(newRow).find('input').css({"width": cellWidth});
+         });
+    }
+    else {
+         $('#tableRecords tbody').append($('<tr>'));
+         var newRow = $('#tableRecords tbody').find('tr').last();
+         var message = 'You have no '+activeBtnState.sectionName+' added yet!';
+         setNoRecordsBody(newRow, message);
+    }
+}
 
-            for (var key in elem) {
-                if(key==="pk") {
-                    continue;
-                }
-                if(key==="car") {
-                    if(!isSelectedCar(elem[key])) {
-                        if(!carHasRecords && index === (self.results.length-1)) {
-                            numOfColumns = $(tableHead).find('th').length;
-                            $(newRow).append($('<td>')
-                                        .attr('colspan', numOfColumns)
-                                        .append($('<div>')
-                                            .attr('id', 'noRecordsMessage')
-                                            .text('You have no '+self.sectionName+' added yet!')));
-                        }
-                        return; // this acts as continue statement
-                    }
-                    carHasRecords = true;
-                    continue;
-                }
-
-                if(!self.tableHeadCellsAdded) {
-                    var name = getTableHeadName(key);
-                    tableHead.append($('<th>').text(name));
-                }
-                $(newRow).append($('<td>')
-                  .append($('<input>')
-                       .prop('type', 'text')
-                       .attr('name', key)
-                       .val(elem[key])
-                ));
-
-            }
-
-            var tableCell = $('td');
-            var leftPadding = tableCell.css('padding-left');
-            var rightPadding = tableCell.css('padding-right');
-            var widthPadding = parseInt(leftPadding.charAt(0)) + parseInt(rightPadding.charAt(0));
-            var cellWidth = tableContainerWidth/numOfColumns-(widthPadding+1);
-            $(newRow).find('input').css({"width": cellWidth});
-
-            self.tableHeadCellsAdded = true;
-     });
+function setNoRecordsBody(newRow, message) {
+    $(newRow).append($('<td>')
+        .attr('colspan', activeBtnState.numOfColumns)
+        .append($('<div>')
+            .attr('id', 'noRecordsMessage')
+            .text(message)));
 }
 
 function getTableHeadName(key) {
@@ -752,7 +771,8 @@ function isSelectedCar(carUrl) {
 
 function selectCarHandler() {
     removeTableRows();
-    tableStateObj.addNewRow();
+    addTableBody();
+    //tableStateObj.addNewRow();
 }
 
 function removeTableRows() {
