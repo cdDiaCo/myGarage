a2eaa9dbb887ac0239e253e60717f7fdb95c6ec4
claudia <claudia_cocioaba@yahoo.ro>
2015-08-11 18:32:43 +0300

    the table has a min height from now on

--- myGarageClient/static/myGarage/js/myscripts.js
+++ myGarageClient/static/myGarage/js/myscripts.js
@@ -17,6 +17,7 @@ function getCookie(name) {
 
 var activeBtnState = {};
 
+// when one of the section buttons gets clicked
 function setButtonActive(elem) {
     deactivateButtons();
     $(elem).addClass('active');
@@ -24,6 +25,7 @@ function setButtonActive(elem) {
     prepareTable();
 }
 
+// before activating a new button, we have to deactivate the last one
 function deactivateButtons() {
     var buttons  = $('.sectionsButton');
     var id;
@@ -35,7 +37,8 @@ function deactivateButtons() {
     });
 }
 
-
+// this gets called as soon as the page loads
+// and it populates the cars select box
 function getUserCars() {
     var carNum = 0;
     $.ajax({type:"GET", url: "/api/v1/cars/"})
@@ -59,9 +62,9 @@ function getUserCars() {
         });
 }
 
+// check what section button has the active class
+// generate table for that section
 function prepareTable() {
-    // check what section button has the active class
-    //generate table for that section
     $('#tableRecords').css({'display': 'none'});
     var sectionBtn = $('.sectionsButton.active');
     var sectionID = $(sectionBtn).attr('id');
@@ -69,10 +72,9 @@ function prepareTable() {
     var sectionName = sectionID.substring(0, index);
     sectionName = sectionName.toLowerCase();
     getTableColumns(sectionName);
-
 }
 
-
+// gets the records for the selected section
 function getTableRecords(sectionName) {
      $.ajax({type:"GET", url: "/api/v1/"+sectionName+"/"})
         .fail(function(resp){
@@ -86,6 +88,7 @@ function getTableRecords(sectionName) {
      });
 }
 
+// gets the columns for the selected section
 function getTableColumns(sectionName) {
     var sectionSingular;
     if(sectionName === "taxes") {
@@ -106,6 +109,7 @@ function getTableColumns(sectionName) {
     });
 }
 
+// this builds the table's head
 function addTableHead() {
     $('#tableRecords thead').append($('<tr>'));
     var tableHead = $('#tableRecords thead').find('tr');
@@ -119,22 +123,25 @@ function addTableHead() {
     tableHead.append($('<th>').css({'width': '70px'}));
 }
 
+// this builds the table's body
 function addTableBody() {
-    if(activeBtnState.numOfRecords > 0){
+    if(activeBtnState.numOfRecords > 0){ // there are records for this section
+         var noRecordsMsg = "";
          var carHasRecords = false;
          $.each(activeBtnState.results, function(index, elem) {
              var tableContainerWidth = $('#garageContent').width();
-             $('#tableRecords tbody').append($('<tr>'));
-             var newRow = $('#tableRecords tbody').find('tr').last();
+             $('#contentBody').append($('<tr>'));
+             var newRow = $('#contentBody').find('tr').last();
              for (var key in elem) {
                  if(key==="pk") {continue;}
                  if(key==="car") {
-                      if(!isSelectedCar(elem[key])) {
+                      if(!isSelectedCar(elem[key])) { // this record belongs to another car
                           if(!carHasRecords && index === (activeBtnState.results.length-1)) {
-                              var message = 'You have no '+activeBtnState.sectionName+' added for this car!';
-                              setNoRecordsBody(newRow, message);
+                              // this combination of section and car doesn't have any records
+                              noRecordsMsg = 'You have no '+activeBtnState.sectionName+' added for this car!';
+                              setNoRecordsBody(newRow, noRecordsMsg);
                           }
-                          return;
+                          return; // skip to the next record
                       }
                       carHasRecords = true;
                       continue;
@@ -156,12 +163,33 @@ function addTableBody() {
              $(newRow).find('input').css({"width": cellWidth});
          });
 
+        // here we remove the table row/rows added and not populated
+        // because the corresponding record/records belong/s to another car (the user has more than one car)
+        $('#contentBody tr').each(function(index, elem){
+                if($(elem).children().length == 0) {
+                    $(elem).remove();
+                }
+        });
 
+         // here we add an extra tbody that holds an 'empty space'
+         // the purpose is to to make up for the difference between the min height of the table body (150px) and
+         // the height that the table would have with only 2-3 rows
+         if(!noRecordsMsg) { // we have records for the selected section and car
+             var contentBodyHeight = $("#contentBody tr").length * $('#contentBody tr').height();
+             if(contentBodyHeight < 150){
+                var emptySpace = 150 - contentBodyHeight;
+                $("#emptyBody").append($('<tr>')
+                                    .append($('<td>').attr('colspan', activeBtnState.numOfColumns+1)
+                                        .append($('<div>')
+                                            .attr('id', 'emptyDiv'))));
+                $("#emptyDiv").height(emptySpace);
+             }
+         }
     }
-    else {
+    else { // there are no records for this section
          $('#tableRecords thead tr').children().last().remove();
-         $('#tableRecords tbody').append($('<tr>'));
-         var newRow = $('#tableRecords tbody').find('tr').last();
+         $('#contentBody').append($('<tr>'));
+         var newRow = $('#contentBody').find('tr').last();
          var message = 'You have no '+activeBtnState.sectionName+' added yet!';
          setNoRecordsBody(newRow, message);
     }
@@ -170,6 +198,7 @@ function addTableBody() {
     setAddNewRecordBtn();
 }
 
+// adds the button responsible for adding a new row
 function setAddNewRecordBtn() {
     var tableHeight = $('#tableRecords').height();
     var marginTop = 15 + tableHeight + "px";
@@ -177,6 +206,7 @@ function setAddNewRecordBtn() {
     $('#addNewRecordBtn').css({'visibility': 'visible'});
 }
 
+// adds an additional column that holds the save and delete row buttons
 function addOperationsButtons(newRow) {
      $(newRow).append($('<td>')
             .css({'width': '70px'})
@@ -189,6 +219,7 @@ function addOperationsButtons(newRow) {
                     .attr("src", deleteImgSrc))));
 }
 
+// this is called when the user has no records for a certain section and/or car
 function setNoRecordsBody(newRow, message) {
     $(newRow).append($('<td>')
         .attr('colspan', activeBtnState.numOfColumns+1)
@@ -197,6 +228,7 @@ function setNoRecordsBody(newRow, message) {
             .text(message)));
 }
 
+// get table head columns names from the DB column names
 function getTableHeadName(key) {
     var res = key.split("_");
     var name ="";
@@ -210,6 +242,7 @@ function getTableHeadName(key) {
     return name;
 }
 
+// this checks if the car selected by the user matches the current record's car
 function isSelectedCar(carUrl) {
     carUrl = String(carUrl);
     var carID = carUrl.charAt(carUrl.length-2);
@@ -220,11 +253,10 @@ function isSelectedCar(carUrl) {
     return false;
 }
 
-
+// this is called when the user selects a different car
 function selectCarHandler() {
     removeTableRows();
     addTableBody();
-    //tableStateObj.addNewRow();
 }
 
 function removeTableRows() {
